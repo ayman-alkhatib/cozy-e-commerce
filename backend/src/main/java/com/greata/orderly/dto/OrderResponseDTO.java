c8466224501f4aec4a49639b591d3798c59c2a6c
Ayman ALKHATIB <aemn22384@gmail.com>
2025-02-12 12:29:31 +0100

    Add DTO classes for Order, OrderItem, and OrderResponse to handle order processing

---
+++ backend/src/main/java/com/greata/orderly/dto/OrderResponseDTO.java
@@ -0,0 +1,33 @@
+package com.greata.orderly.dto;
+
+import java.util.List;
+
+public class OrderResponseDTO {
+    private long orderId;
+    private String email;
+    private List<OrderResponseItemDTO> items;
+    private double total;
+
+    public OrderResponseDTO(String email, List<OrderResponseItemDTO> items, double total) {
+        this.email = email;
+        this.items = items;
+        this.total = total;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<OrderResponseItemDTO> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderResponseItemDTO> items) {
+        this.items = items;
+    }
+}
+
