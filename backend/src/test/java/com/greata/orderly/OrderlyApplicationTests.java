4d06afee895debd6c27b52ebae11aeff4eaab0eb
Ayman ALKHATIB <aemn22384@gmail.com>
2025-02-12 12:12:59 +0100

    Initialize backend structure with Maven, Spring Boot, and basic test setup

---
+++ backend/src/test/java/com/greata/orderly/OrderlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greata.orderly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
