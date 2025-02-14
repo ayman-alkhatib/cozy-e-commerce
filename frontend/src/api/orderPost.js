23b73d7e2fd99cf64cbf49fcdf920131df66ea28
Ayman ALKHATIB <aemn22384@gmail.com>
2025-02-14 12:13:52 +0100

    Remove debug logs from fetchAllOrdersByEmail and orderPost API functions

--- frontend/src/api/orderPost.js
+++ frontend/src/api/orderPost.js
@@ -5,10 +5,7 @@ export default async function orderPost(order) {
                 "Content-Type": "application/json"
             }, body: JSON.stringify(order)
         })
-        console.log(res)
         if (!res.ok) {
-            console.log(res.statusText)
-            console.log(await res.json())
             throw Error("Failed to fetch products list data. Please try again later.")
         }
         return res
